beed1e332a3359d85c4de27c7a9a79310d73fab2
christiangoran <christian.goran@protonmail.com>
2023-10-24 11:48:55 +0200

    added authentication for signup form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,19 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../../styles/SignUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import signUpImage from "../../assets/signup.webp";
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useNavigate();
+
+  const handleChange = (e) => {
+    setSignUpData({
+      ...signUpData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        "https://crypto-tracker-heroku-b5acda38c706.herokuapp.com/dj-rest-auth/registration/",
+        signUpData
+      );
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data || {});
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Sign Up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -21,11 +61,19 @@ const SignUpForm = () => {
                 type="text"
                 placeholder="Enter Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
               <Form.Text className={styles.FormText}>
                 We'll never share your information with anyone else.
               </Form.Text>
             </Form.Group>
+            {errors &&
+              errors.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
             <Form.Group className="mb-3" controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
@@ -34,8 +82,17 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="Password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors &&
+              errors.password1?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+
             <Form.Group className="mb-3" controlId="password2">
               <Form.Label className="d-none">Confirm Password</Form.Label>
               <Form.Control
@@ -43,8 +100,16 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="Confirm Password"
                 name="password2"
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors &&
+              errors.password2?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
             <Button
               className={`${btnStyles.Button} ${btnStyles.Dark}`}
@@ -52,6 +117,12 @@ const SignUpForm = () => {
             >
               Sign Up
             </Button>
+            {errors &&
+              errors.non_field_errors?.map((message, idx) => (
+                <Alert variant="warning" key={idx} className="mt-3">
+                  {message}
+                </Alert>
+              ))}
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
