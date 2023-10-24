e3fdb04a02b360b3e57d81220ec47924560f8b1a
christiangoran <christian.goran@protonmail.com>
2023-10-24 09:16:10 +0200

    added styling to the form and buttons

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -13,7 +13,46 @@ const SignUpForm = () => {
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Sign Up</h1>
 
-          {/* add your form here */}
+          <Form>
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+              />
+              <Form.Text className={styles.FormText}>
+                We'll never share your information with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password2">
+              <Form.Label className="d-none">Confirm Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+              />
+            </Form.Group>
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Dark}`}
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
