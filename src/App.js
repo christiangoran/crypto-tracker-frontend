5559109e8e8bf47594cb82ff03d6d8adf99c7c19
christiangoran <christian.goran@protonmail.com>
2023-10-25 10:20:29 +0200

    added login functionality and integratet API call for authentication

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Routes } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Container className={styles.Main}>
         <Routes>
           <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/signin" element={<h1>Sign In</h1>} />
+          <Route path="/signin" element={<SignInForm />} />
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/cryptocurrencies" element={<h1>Cryptocurrencies</h1>} />
           <Route path="/forum" element={<h1>Forum</h1>} />
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,9 +1,4 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import styles from "../../styles/SignInUpForm.module.css";
-import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css";
-import signUpImage from "../../assets/rocket.gif";
 import {
   Form,
   Button,
@@ -13,42 +8,46 @@ import {
   Container,
   Alert,
 } from "react-bootstrap";
+import loginImage from "../../assets/login.webp";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
 import axios from "axios";
 
-const SignUpForm = () => {
-  const [signUpData, setSignUpData] = useState({
+function SignInForm() {
+  const [signInData, setSignInData] = useState({
     username: "",
-    password1: "",
-    password2: "",
+    password: "",
   });
-  const { username, password1, password2 } = signUpData;
+
+  const { username, password } = signInData;
 
   const [errors, setErrors] = useState({});
-
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/registration/", signUpData);
-      navigate("/signin");
+      await axios.post("/dj-rest-auth/login/", signInData);
+      navigate("/");
     } catch (err) {
       setErrors(err.response?.data);
     }
   };
 
   const handleChange = (e) => {
-    setSignUpData({
-      ...signUpData,
+    setSignInData({
+      ...signInData,
       [e.target.name]: e.target.value,
     });
   };
 
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Sign Up</h1>
+          <h1 className={styles.Header}>Sign In</h1>
 
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="username">
@@ -62,7 +61,7 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
               <Form.Text className={styles.FormText}>
-                We'll never share your information with anyone else.
+                We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
             {errors.username?.map((message, idx) => (
@@ -71,35 +70,18 @@ const SignUpForm = () => {
               </Alert>
             ))}
 
-            <Form.Group className="mb-3" controlId="password1">
+            <Form.Group className="mb-3" controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
                 className={styles.Input}
                 type="password"
                 placeholder="Password"
-                name="password1"
-                value={password1}
+                name="password"
+                value={password}
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors.password1?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-
-            <Form.Group className="mb-3" controlId="password2">
-              <Form.Label className="d-none">Confirm Password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="Confirm Password"
-                name="password2"
-                value={password2}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password2?.map((message, idx) => (
+            {errors.password?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
@@ -109,7 +91,7 @@ const SignUpForm = () => {
               className={`${btnStyles.Button} ${btnStyles.Dark}`}
               type="submit"
             >
-              Sign Up
+              Sign In
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
               <Alert variant="warning" key={idx} className="mt-3">
@@ -119,19 +101,19 @@ const SignUpForm = () => {
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
           </Link>
         </Container>
       </Col>
       <Col
         md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
       >
-        <Image className={`${appStyles.FillerImage}`} src={signUpImage} />
+        <Image className={`${appStyles.FillerImage}`} src={loginImage} />
       </Col>
     </Row>
   );
-};
+}
 
-export default SignUpForm;
+export default SignInForm;
